7eba6eee2d27fdedbe0b2c636deb004cb3bce562
MatthewLeaman <leaman.30@osu.edu>
2024-01-19 01:24:10 -0500

    added more info button to each team after listing

--- project.js
+++ project.js
@@ -15,7 +15,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
         .then(response => response.json())
         .then(datas => listingEast(datas))
         })
+    document.querySelector("form").addEventListener("submit", () => {
+        fetch('https://www.balldontlie.io/api/v1/teams')
+        .then(response => response.json())
+        .then(divisions => divisionSearch(divisions))
     })
+})
 
 
 
@@ -28,14 +33,32 @@ function listingWest(datas) {
     demo.forEach(team => {
         if (team.conference === "West") {
             const p = document.createElement("p");
-            p.innerHTML = team.full_name;
+            const btn = document.createElement('button');
+            btn.textContent = "More Info"
+            btn.addEventListener("click", () => {
+                const information = [];
+                information[0] = "Abbreviation: " + team.abbreviation;
+                information[1] = "City: " + team.city;
+                information[2] = "Division: " + team.division;
+                information[3] = "Mascot Name: " + team.name;
+                p.textContent = team.full_name + " Information:  ";
+                for (let j = 0; j < 4; j++) {
+                    if(j < 3) {
+                        p.append(information[j] + ",  ")
+                    }
+                    else {
+                        p.append(information[j])
+                    }
+                }
+            })
+            p.textContent = team.full_name + "   ";
+            p.appendChild(btn);
             list.appendChild(p);
         }
     })
 }
 
 function listingEast(datas) {
-
     let demo = [];
     demo = datas.data;
     const list = document.querySelector("main");
@@ -43,8 +66,31 @@ function listingEast(datas) {
     demo.forEach(team => {
         if (team.conference === "East") {
             const p = document.createElement("p");
-            p.innerHTML = team.full_name;
+            const btn = document.createElement('button');
+            btn.textContent = "More Info"
+            btn.addEventListener("click", () => {
+                const information = [];
+                information[0] = "Abbreviation: " + team.abbreviation;
+                information[1] = "City: " + team.city;
+                information[2] = "Division: " + team.division;
+                information[3] = "Mascot Name: " + team.name;
+                p.textContent = team.full_name + " Information:  ";
+                for (let j = 0; j < 4; j++) {
+                    if(j < 3) {
+                        p.append(information[j] + ",  ")
+                    }
+                    else {
+                        p.append(information[j])
+                    }
+                }
+            })
+            p.textContent = team.full_name + "   ";
+            p.appendChild(btn);
             list.appendChild(p);
         }
     })
+}
+
+function divisionSearch(divisions) {
+    
 }
